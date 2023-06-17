a56e3c2c57cabe2c8aa37f734618a7468a4893de
PNGHA <93396414+PNg-HA@users.noreply.github.com>
2023-06-17 20:58:36 +0700

    My personal bai_2

---
+++ Bai_2/Form1.Designer.cs
@@ -0,0 +1,73 @@
+﻿
+namespace Bai_2
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.ListenBut = new System.Windows.Forms.Button();
+            this.MessageTB = new System.Windows.Forms.RichTextBox();
+            this.SuspendLayout();
+            // 
+            // ListenBut
+            // 
+            this.ListenBut.Location = new System.Drawing.Point(327, 324);
+            this.ListenBut.Name = "ListenBut";
+            this.ListenBut.Size = new System.Drawing.Size(90, 46);
+            this.ListenBut.TabIndex = 0;
+            this.ListenBut.Text = "Listen";
+            this.ListenBut.UseVisualStyleBackColor = true;
+            this.ListenBut.Click += new System.EventHandler(this.ListenBut_Click);
+            // 
+            // MessageTB
+            // 
+            this.MessageTB.Location = new System.Drawing.Point(24, 122);
+            this.MessageTB.Name = "MessageTB";
+            this.MessageTB.Size = new System.Drawing.Size(393, 175);
+            this.MessageTB.TabIndex = 1;
+            this.MessageTB.Text = "";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(436, 450);
+            this.Controls.Add(this.MessageTB);
+            this.Controls.Add(this.ListenBut);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button ListenBut;
+        private System.Windows.Forms.RichTextBox MessageTB;
+    }
+}
+
---
+++ Bai_2/Form1.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Threading;
+using System.Net;
+using System.Net.Sockets;
+namespace Bai_2
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+        public void unsafeThread()
+        {
+            int bytesRcv = 0;
+
+            byte[] recv = new byte[1];
+            Socket clSocket;
+            Socket listenerSocket = new Socket(
+                AddressFamily.InterNetwork,
+                SocketType.Stream,
+                ProtocolType.Tcp
+            );
+            IPEndPoint ipSv = new IPEndPoint(IPAddress.Any, 8080);
+
+            listenerSocket.Bind(ipSv);
+            //start to listen
+            listenerSocket.Listen(-1);
+
+            clSocket = listenerSocket.Accept();
+            // Rcv data
+            MessageTB.Text += "New client" + '\n';
+            while (clSocket.Connected)
+            {
+                string text = "";
+                do
+                {
+                    bytesRcv = clSocket.Receive(recv);
+                    text += Encoding.ASCII.GetString(recv);
+                } while (text[text.Length - 1] != '\n');
+                MessageTB.Text += text + '\n';
+            }
+        }
+        private void ListenBut_Click(object sender, EventArgs e)
+        {
+            CheckForIllegalCrossThreadCalls = false;
+            Thread svThread = new Thread(
+                new ThreadStart(unsafeThread)
+            );
+            svThread.Start();
+        }
+    }
+}
